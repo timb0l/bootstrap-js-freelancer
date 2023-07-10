0ef7fefa7272f00cb0006b0c54928d7ad1b8843e
Jam Timbol <jam.timbol00@gmail.com>
2023-07-10 10:20:19 +0200

    Front ok

---
+++ main.js
@@ -0,0 +1,11 @@
+// // Il prezzo orario per una commissione varia in questo modo:
+// - se la commissione riguarda lo sviluppo backend il prezzo orario è di 20.5€ l’ora
+// - se la commissione riguarda lo sviluppo frontend il prezzo orario è di 15.3€ l’ora
+// - se la commissione riguarda l’analisi progettuale di un progetto il prezzo orario è di 33.6€
+
+// L’utente potrebbe decidere di utilizzare un codice promozionale tra i seguenti:
+// - YHDNU32
+// - JANJC63
+// - PWKCN25
+// - SJDPO96
+// - POCIE24
